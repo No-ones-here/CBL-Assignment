78120ec349288df5ace814b274e99627f49ff62f
No-ones-here <luissantos0016@gmail.com>
2023-10-12 11:57:45 +0200

    Entity, State, Window basic constructor

--- Window/Window.java
+++ Window/Window.java
@@ -1,5 +1,19 @@
 package Window;
 
+import javax.swing.*;
+
 public class Window extends JFrame {
-    
+    private int width;
+    private int height;
+    private String title;
+
+    public Window(int width, int height, String title) {
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+
+    public void createWindow() {
+        
+    }
 }
