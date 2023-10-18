f5f41e446d8152fbd7fb1c6ac705cf80be091dff
No-ones-here <luissantos0016@gmail.com>
2023-10-18 13:09:16 +0200

    Implemented Getter Methods in Handler

--- Launcher/Handler.java
+++ Launcher/Handler.java
@@ -1,11 +1,27 @@
 package Launcher;
+import Window.*;
+import InputHandlers.*;
 
 public class Handler {
-    private Game game;
+    private static Game game;
 
     public Handler(Game g) {
         this.game = g;
     } 
 
-    
+    public int getWidth() {
+        return game.getWidth();
+    }
+    public int getHeight() {
+        return game.getHeight();
+    }
+    public Window getWindow() {
+        return game.getWindow();
+    }
+    public KeyHandler getKeyHandler() {
+        return game.getkH();
+    }
+    public MouseHandler getMouseHandler() {
+        return game.getmH();
+    }
 }
