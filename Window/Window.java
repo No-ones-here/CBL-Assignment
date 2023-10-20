3b6a341aa29bb7144d3e5cd37c51b466b0985da6
danieleguggio <ilguggio6@gmail.com>
2023-10-20 14:02:15 +0200

    Window/Window.java animation road loop
    Co-authored-by: Santos, Luis <a.l.santos@student.tue.nl>

--- Window/Window.java
+++ Window/Window.java
@@ -8,10 +8,12 @@ import java.util.ArrayList;
 
 public class Window {
     
- 
+   
     public void createWindow() {
         
     }
+
+    
 
     public void runGraph() {
         
@@ -23,72 +25,63 @@ public class Window {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
 
+       
         
         // Imported images and sprite
         ImageIcon background = new ImageIcon("\\Users\\20233231\\Pictures\\Saved Pictures\\OOWbackground3.jpg");
         ImageIcon road = new ImageIcon("\\Users\\20233231\\Pictures\\Saved Pictures\\OOWroad2.jpg");
+        ImageIcon road2 = new ImageIcon("\\Users\\20233231\\Pictures\\Saved Pictures\\OOWroad2.jpg");
         ImageIcon pilot = new ImageIcon("C:\\Users\\20233231\\Pictures\\Saved Pictures\\PinClipart.com_reason-clipart_821299.png");
         
         // labels
         JLabel pilotlabel = new JLabel(pilot);
         JLabel roadlabel = new JLabel(road);
+        JLabel roadlabel2 = new JLabel(road2); 
         JLabel backgroundlabel = new JLabel(background);
-        
 
-        // Panel that contains the background and the road
-        JPanel panel = new JPanel(); 
-        panel.add(backgroundlabel);
-        panel.add(roadlabel);
-
-        // puts the road right under the background
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)); 
-
-        backgroundlabel.setSize(800, 600);
-
-        
-        // Box that contains the panel
-        // to be collocated at the centre
-        Box box = Box.createVerticalBox();
-        box.add(Box.createVerticalGlue());
-        box.add(panel);
-        box.add(Box.createVerticalGlue());
+        roadlabel2.setLocation(road.getIconWidth(), 200 + background.getIconHeight());
 
         // Animation: looping the road
-        Timer timer = new Timer(100, new ActionListener() {
+        Timer timer = new Timer(1, new ActionListener() {
             int roadx = 0;
+            int roadx2 = road.getIconWidth();
+            
             @Override
-            public void actionPerformed(ActionEvent e){
-                ArrayList<ImageIcon> loops = new ArrayList<>();
-                loops.add(1, road);
-                loops.add(2, road);
-                roadx = (roadx - 5) % road.getIconWidth();
-                roadlabel.setBounds(roadx, 0, road.getIconWidth(), road.getIconHeight());
+            public void actionPerformed(ActionEvent e) {
+                backgroundlabel.setLocation(0, 200);
                 
-                /* 
-                for (int i = 1; i <= 2; i++) {
-                    if (i < 2) {
-                        roadx = (roadx - 5) % loops.get(i).getIconWidth();
-                        roadlabel.setBounds(roadx, 0, loops.get(i).getIconWidth(), loops.get(i).getIconHeight());
-                    } else if (i == 2) {
-                        i = 1;
-                        roadx = (roadx - 5) % loops.get(i).getIconWidth();
-                        roadlabel.setBounds(roadx, 0, loops.get(i).getIconWidth(), loops.get(i).getIconHeight());
-                    }
+                roadx = (roadx - 5) % road.getIconWidth();
+                roadlabel.setLocation(roadx, 200 + background.getIconHeight());
+
+                if (roadx2 == 0) {
+                    roadx2 = road.getIconWidth();
+                    roadx2 = (roadx2 - 5) % road2.getIconWidth();
+                    roadlabel2.setLocation(roadx2, 200 + background.getIconHeight());
+                } else if (roadx2 > 0) {
+                    roadx2 = (roadx2 - 5) % road2.getIconWidth();
+                    roadlabel2.setLocation(roadx2, 200 + background.getIconHeight());
                 }
-                */
+            
             }
+            
         });
+        
 
-
-        frame.setContentPane(box);
-        frame.setBackground(Color.ORANGE);
+        timer.start();
+        
+        frame.add(wheelie);
+        frame.add(jump, BorderLayout.NORTH);
+        frame.add(backgroundlabel);
+        frame.add(roadlabel);
+        frame.add(roadlabel2);
+        frame.getContentPane().setBackground(Color.ORANGE);
         pilotlabel.setBackground(Color.lightGray);
         frame.setVisible(true);
 
         
         
     }
-
+    
     public static void main(String[] args) {
         new Window().runGraph();
     }
