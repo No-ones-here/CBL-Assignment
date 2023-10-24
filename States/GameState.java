8a6c0ad758cac99f853231a9c78646ac56077aa1
No-ones-here <luissantos0016@gmail.com>
2023-10-24 13:54:17 +0200

    Added Imports and Objects

--- States/GameState.java
+++ States/GameState.java
@@ -1,5 +1,10 @@
 package States;
-import Entities.*;
+
+import Entities.Ground;
+import Entities.Player;
+import Launcher.Handler;
+import ObstacleHandler.ObstacleList;
+import World.World;
 
 /**
  * Handles Events while the game is running.
@@ -7,16 +12,19 @@ import Entities.*;
  * @author Luis Santos
  * @id 1998544
  */
-public class GameState extends State{
-    private Player player;
+public class GameState extends State {
+    private Handler handler;
+    private World world;
 
-    public GameState() {
-        
+    public GameState(Handler handler) {
+        this.handler = handler;
+        this.world = new World(handler);
+        handler.setWorld(world);
     }
 
     @Override
     public void run() {
         // TODO: Add Game Processes
+
     }
-    
 }
