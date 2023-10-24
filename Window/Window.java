53df031b979b21a1838211adfb877840e1fd3088
danieleguggio <ilguggio6@gmail.com>
2023-10-24 15:05:42 +0200

    window perfectly running with classes implemented

--- Window/Window.java
+++ Window/Window.java
@@ -1,5 +1,6 @@
 package Window;
 
+// Imports
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -7,23 +8,25 @@ import java.awt.event.ActionListener;
 
 
 public class Window {
-    Animation anim = new Animation() 
-    // Frame
-    JFrame frame = new JFrame("Wheelie it up!");
+    Animation anim = new Animation(this);
+    
 
     // Buttons
     JButton wheelie = new JButton("Wheelie", null);
     JButton jump = new JButton("jump", null);
-    
+    JButton startGame = new JButton("START GAME");
+    JButton instructions = new JButton("INSTRUCTIONS");
  
     // Labels
-    JLabel pilotlabel = new JLabel(anim.getimg().getpilot());
-    JLabel roadlabel = new JLabel(road);
-    JLabel roadlabel2 = new JLabel(road2); 
-    JLabel backgroundlabel = new JLabel(background);
-    JLabel instructions = new JLabel("Press: W to wheelie " + "or J to jump");
+    JLabel pilotlabel = new JLabel(anim.getimg().getPilot());
+    JLabel roadlabel = new JLabel(anim.getimg().getRoad());
+    JLabel roadlabel2 = new JLabel(anim.getimg().getRoad2()); 
+    JLabel backgroundlabel = new JLabel(anim.getimg().getBackground());
+    JLabel instruction = new JLabel("Press: W to wheelie " + "or J to jump");
+    JLabel bgmenulabel = new JLabel(anim.getimg().getBgMenu());
+    
 
-   
+    // Constructor method
     public Window() {
     }
     
@@ -56,55 +59,21 @@ public class Window {
     
 
 
-    public void runGraph() {
-        
-       
-        
+    public void runGame() {
+        JFrame frame = new JFrame("Wheelie it up!");
         frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
 
         
-        roadlabel2.setLocation(road.getIconWidth(), 200 + background.getIconHeight());
+        roadlabel2.setLocation(anim.getimg().getRoad().getIconWidth(), 200 + anim.getimg().getBackground().getIconHeight());
 
-        // Animation: looping the road
-        Timer timer = new Timer(1, new ActionListener() {
-            int roadx = 0;
-            int roadx2 = road.getIconWidth();
-            
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                backgroundlabel.setLocation(0, 200);
-                wheelie.setLocation(600, 350 + background.getIconHeight());
-                jump.setLocation(800, 350 + background.getIconHeight());
-                pilotlabel.setLocation(50, 175 + background.getIconHeight());
-                
-
-                // Move first road to the left
-                roadx = (roadx - 5) % road.getIconWidth();
-                roadlabel.setLocation(roadx, 200 + background.getIconHeight());
-                
-                // Move second road to the left
-                // If the second road goes off the window
-                if (roadx2 == 0) {    //start again from the original position
-                    roadx2 = road.getIconWidth();                       
-                    roadx2 = (roadx2 - 5) % road2.getIconWidth();                     
-                    roadlabel2.setLocation(roadx2, 200 + background.getIconHeight());           
-                } else if (roadx2 > 0) {
-                    roadx2 = (roadx2 - 5) % road2.getIconWidth();
-                    roadlabel2.setLocation(roadx2, 200 + background.getIconHeight());
-                }
-            
-            }
-            
-        });
-        
-
-        timer.start();
-        
+        // Looping the road
+        anim.runAnimation();
+       
         // Adding components to the frame 
         frame.add(pilotlabel);
-        frame.add(instructions, BorderLayout.NORTH);
+        frame.add(instruction, BorderLayout.NORTH);
         frame.add(wheelie);
         frame.add(jump, BorderLayout.NORTH);
         frame.add(backgroundlabel);
@@ -114,8 +83,25 @@ public class Window {
         frame.getContentPane().setBackground(Color.ORANGE);
         frame.setVisible(true);
     }
+
+    public void runMenu() {
+        JFrame frame2 = new JFrame("Welocme to OOW", null);
+        frame2.setSize(800, 600);
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+        // bgmenulabel.setLocation(0, 250);
+
+        // frame2.add(bgmenulabel);
+
+        frame2.setBackground(Color.ORANGE);
+        frame2.setVisible(true);
+
+    }
+
+
     
     public static void main(String[] args) {
-        new Window(800, 600, "Wheelie it up!").runGraph();
+        new Window().runGame();
     }
 }
