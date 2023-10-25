5dd8f3cd59392bac2bbbd642e53b8349aaf38e81
No-ones-here <luissantos0016@gmail.com>
2023-10-25 17:29:50 +0200

    Added World Class

--- World/World.java
+++ World/World.java
@@ -1,10 +1,12 @@
 package World;
 
+import Entities.Background;
 import Entities.Ground;
 import Entities.Obstacle;
 import Entities.Player;
 import Launcher.Handler;
 import ObstacleHandler.ObstacleList;
+import Window.Images;
 
 /**
  * Describes the world that the game takes place.
@@ -18,6 +20,8 @@ public class World {
     private Ground g2;
     private ObstacleList obsList;
     private Obstacle nearestObstacle;
+    private Background bg;
+    private Images images;
     //TODO: Add Images!
 
     /**
@@ -27,10 +31,12 @@ public class World {
      */
     public World(Handler handler) {
         //TODO: Implement Images!!!
-        this.player = new Player(50, 250, null, handler);
-        this.g1 = new Ground(0, 300, null, handler);
-        this.g2 = new Ground(g1.getxSize(), 300, null, handler);
+        this.images = new Images();
+        this.player = new Player(50, 250, images.getpilot(), handler);
+        this.g1 = new Ground(0, 300, images.getroad(), handler);
+        this.g2 = new Ground(g1.getxSize(), 300, images.getroad(), handler);
         this.obsList = new ObstacleList(handler);
+        this.bg = new Background();
     }
 
 
