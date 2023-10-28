f055166f67aab869a519b32f2eceac4231156d88
No-ones-here <luissantos0016@gmail.com>
2023-10-28 14:16:26 +0200

    Improved Obstacle Generation

--- ObstacleHandler/ObstacleList.java
+++ ObstacleHandler/ObstacleList.java
@@ -18,37 +18,74 @@ public class ObstacleList {
     private Handler handler;
     private ImageIcon sprite;
     private int defaultHeight;
-    private int intitalStepSize;
+    private int currentStepSize;
 
+    /**
+     * Constructor method that initializes the instance variables of the
+     * class.
+     * 
+     * @param handler object that allows access to vars in Game class
+     * @param sprite image that represents the obstacles
+     * @param defaultHeight top-left height of the obstacles
+     * @param initalStepSize starting step size of the obstacle
+     */
     public ObstacleList(Handler handler, ImageIcon sprite,
                          int defaultHeight, int initalStepSize) {
 
         obstacles = new LinkedList<Obstacle>();
         this.handler = handler;
         this.defaultHeight = defaultHeight;
-        this.intitalStepSize = - initalStepSize;
+        this.currentStepSize = - initalStepSize;
         this.sprite = sprite;
 
     }
 
-    //TODO: Implement Obstacle Generation.
+    /**
+     * Generates 5 obstacles in a Linked List. If an obstacle moves out of the
+     * frame, it is removed and another is generated at other end of the frame behind
+     * the last obstacle.
+     * 
+     */
     public void generateObstacles() {
         Random rand = new Random();
+        Obstacle previous;
+        int bottomLimit;
 
         if (!(obstacles.isEmpty()) && obstacles.peekFirst().getX() < -sprite.getIconWidth()) {
             obstacles.removeFirst();
         }
         
         while (obstacles.size() < 5) {
-            obstacles.addLast(new Obstacle((handler.getWidth())
-                            + (rand.nextInt(sprite.getIconWidth() + 700) + sprite.getIconWidth()),
-                            defaultHeight + 10, sprite, handler, intitalStepSize));
+            //Empty List Check
+            if (obstacles.isEmpty()) {
+                obstacles.addLast(new Obstacle(handler.getWidth(), defaultHeight + 10,
+                                     sprite, handler, currentStepSize));
+            }
+
+            //Generate Obstacle in order
+            previous = obstacles.getLast();
+            bottomLimit = previous.getX() + previous.getxSize();
+            
+            //Check for on-screen bottom limit.
+            if (bottomLimit < handler.getWidth()) {
+                bottomLimit = handler.getWidth();
+            }
+
+            //Generate and Add new obstacle into list.
+            obstacles.addLast(new Obstacle(rand.nextInt(500) + bottomLimit, 
+                                            defaultHeight + 10, sprite, handler,
+                                            currentStepSize));
         }
     }
 
-    public void stepAll() {
+    /**
+     * Method that runs every time the game ticks.
+     * Updates the position variables of the obstacles stored in the class.
+     * 
+     */
+    public void tick() {
         for (Obstacle o : obstacles) {
-            o.stepX();
+            o.tick();
         }
     }
 
